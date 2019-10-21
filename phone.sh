02eaf33afae3bdd01c6940ee5a01f6cccd0fc9ab
Sriram Srinivasan <42544518+SriramSrinivas@users.noreply.github.com>
2019-10-20 19:45:21 -0500

    Add files via upload

---
+++ phone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#while true;
+#do
+ curl 'https://api.twilio.com/2010-04-01/Accounts/AC621d2911f177f5839a8432e3433f0544/Messages.json' -X POST --data-urlencode 'To=+14029154185' --data-urlencode 'From=+12512577318' --data-urlencode 'Body=rtr' -u AC621d2911f177f5839a8432e3433f0544:bc23fde051d7d6b0e2f13320f2a8f2d2
+#sleep 120h
+
+#done
---
+++ vpnfinal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+  
+#while true;
+#do
+      
+ping -c1 192.168.0.24 > /dev/null
+ 
+if [ $? -eq 0 ]
+then
+	echo "ok"
+
+else
+command=$(pidof openvpn)
+echo $command
+if [ $command > 0 ]
+then
+  sudo kill $command
+fi
+sudo openvpn --config anvilubuntu.ovpn --daemon
+fi	
+echo "here"
+#sleep 10m
+
+#done
+
